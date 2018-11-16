449f02bfd7333322dee9a017c9f73ad157ae7fe0
Shubhransh <pujasingh@pujas-MacBook-Air.local>
2018-11-17 02:19:31 +0530

    more fixes and improvements

--- resources/js/script.js
+++ resources/js/script.js
@@ -63,6 +63,8 @@ changeWord();
 setInterval(changeWord, 4000);
 
 
+//Stciky Navigation Bar
+
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
@@ -70,11 +72,11 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var trigerOffset = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > trigerOffset) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
